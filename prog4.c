fcd4beafea02aff1974d113822eb6440b628231b
dilippuri <dilippuripuri@gmail.com>
2016-08-09 11:58:17 +0530

    add some more coding program of basic algos and simple questions

--- prog4.c
+++ prog4.c
@@ -3,11 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 
-//void leftrotate(int arr[], size_t size, size_t rotation);
+int main(void){
 
-int main() {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n,d;
     size_t i,j;
 
@@ -18,30 +15,21 @@ int main() {
     for(i=0; i<n; i++){
           arr[i]=i+1;
       }
-for ( j = 0; j < d; j++) {
-      int temp = arr[0];
+    int temp;
+    for(j=0; j<d; j++){
+      temp = arr[0];
       for (i=0; i<n-1;i++){
         arr[i]=arr[i+1];
       }
       arr[n-1]=temp;
-  }
+        for (i=0; i<n;i++){
+        printf("%d ", arr[i]);
+      }printf("\n");
+    }
 
-    for(i=0; i<n; i++){
-          printf("%d ", arr[i] );
-      }
+    //for(i=0; i<n; i++){
+      //    printf("%d ", arr[i] );
+      //}
 
   return 0;
 }
-/*
-void LeftRotate(arr, n, x){
-    int temp;
-    int end;
-    end = n;
-    size_t j;
-    temp = arr[0];
-    for (j=0; j<end-1;j++){
-      arr[j]=arr[j+1];
-    }
-    arr[end-1]=temp;
-    }
-*/
